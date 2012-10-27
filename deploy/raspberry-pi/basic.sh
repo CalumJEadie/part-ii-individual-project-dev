05c6e99cd8db278104448a4a535917e826fa8157
Calum J. Eadie <calum@calumjeadie.com>
2012-10-27 11:22:47 +0100

    Added basic deployment scripts.

---
+++ deploy/raspberry-pi/basic.sh
@@ -0,0 +1,22 @@
+/bin/sh
+
+base_dir=`dirname $0`
+files_dir=$base_dir/files
+
+packages="git-core byobu chromium-browser"
+
+# Install
+sudo apt-get update
+sudo apt-get install $packages
+
+# Configure
+sudo mv /boot/boot_enable_ssh.rc /boot/boot.rc
+sudo mv $files_dir/etc/rc.local /etc/rc.local
+
+# http://git-scm.com/book/en/Customizing-Git-Git-Configuration
+git config --global user.name "CalumJEadie"
+git config --global user.email calum@calumjeadie.com
+git config --global core.editor vim
+git config --global merge.tool vimdiff
+git config --global color.ui true
+
