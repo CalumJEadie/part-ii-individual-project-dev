d830045fdca1536b9b37dc214a459ec33c286ad5
CalumJEadie <calum@calumjeadie.com>
2012-10-28 01:09:17 +0000

    Added versions of jogl demo launch scripts that use youtube

---
+++ research/JoqAmp - Rendering video to surface/run-movie-simple-youtube.sh
@@ -0,0 +1,21 @@
+# Streams video directly from YouTube.
+# Must be run from JOGL root direcotry.
+
+# Usage: _ <format> <youtube url>
+
+if [ $# -ne 2 ]
+then
+	echo "Usage: `basename $0` <format> <youtube url>
+
+See youtube-dl --format parameter for format codes"
+	exit 1
+fi
+
+# Get the url for streaming the video.
+stream_url=`youtube-dl -f $1 -g $2`
+
+echo "Streaming from $stream_url"
+
+java -cp jar/jogl-all.jar:jar/gluegen-rt.jar:/usr/share/java/junit4.jar:jar/jogl-test.jar \
+com/jogamp/opengl/test/junit/jogl/demos/es2/av/MovieSimple \
+-time 400000 -width 1980 -height 1080 -url $stream_url
