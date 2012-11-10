e4b5c1213bcdfcff9cba36cbfde97a1c8926cb68
CalumJEadie <calum@calumjeadie.com>
2012-11-10 13:39:15 +0000

    Added youtube video format experiment using movie cube as a black box

--- research/performance-evaluation-tools/util.py
+++ research/performance-evaluation-tools/util.py
@@ -8,6 +8,20 @@ import time
 
 JOQAMP_DIR = "/opt/diss/jogamp/jogamp-all-platforms"
 
+YOUTUBE_FORMATS = {
+    "5" : "flv [240x400]",
+    "17" : "mp4 [144x176]",
+    "18" : "mp4 [360x640]",
+    "22" : "mp4 [720x1280]",
+    "34" : "flv [360x640]",
+    "35" : "flv [480x854]",
+    "37" : "mp4 [1080x1920]",
+    "43" : "webm [360x640]",
+    "44" : "webm [480x854]",
+    "45" : "webm [720x1280]",
+    "46" : "webm [1080x1920]",
+    }
+
 def get_available_formats(web_url):
 	"""
 	Returns a list of available formats for the specified YouTube web url.
@@ -26,17 +40,12 @@ def run_movie_cube(stream_url):
 	"""
 	Runs the JogAmp/JOGL movie cube demo.
 	"""
+	
 	timeout = 20
-
-	proc = subprocess.Popen(["java","-cp",
+	run_with_timeout(["java","-cp",
 		"jar/jogl-all.jar:jar/gluegen-rt.jar:/usr/share/java/junit4.jar:jar/jogl-test.jar",
 		"com/jogamp/opengl/test/junit/jogl/demos/es2/av/MovieCube",
-		"-time","400000","-width","1980","-height","1080","-url",stream_url],
-		cwd=JOQAMP_DIR)
-	time.sleep(timeout)
-	proc_status = proc.poll()
-	if proc_status is None:
-		proc.terminate()
+		"-time","400000","-width","1980","-height","1080","-url",stream_url],timeout,cwd=JOQAMP_DIR)
 
 def start(args,cwd=None):
 	"""
@@ -46,3 +55,51 @@ def start(args,cwd=None):
 
 def stop(pid):
 	os.kill(pid,signal.SIGKILL)
+	
+def run_with_timeout(cmd,timeout,cwd=None):
+    """
+    Executes "cmd" and terminates after timeout has passed.
+    """
+    proc = subprocess.Popen(cmd,cwd=cwd)
+    time.sleep(timeout)
+    proc_status = proc.poll()
+    if proc_status is None:
+        proc.terminate()
+
+def time_function(f):
+    """
+    Returns the time taken for f to execute.
+    """
+    start = time.time()
+    f()
+    end = time.time()
+    return end-start
+
+def run_timed_movie_cube(stream_url):
+    """
+    Runs the JoqAmp/JOGL movie cube demo and returns how long it took to execute.
+    """
+
+    def f():
+        proc = subprocess.Popen(["java","-cp",
+            "jar/jogl-all.jar:jar/gluegen-rt.jar:/usr/share/java/junit4.jar:jar/jogl-test.jar",
+            "com/jogamp/opengl/test/junit/jogl/demos/es2/av/MovieCube",
+            "-time","400000","-width","1980","-height","1080","-url",stream_url],
+            cwd=JOQAMP_DIR)
+        proc.wait()
+    return time_function(f)
+
+def run_timed_movie_cube_with_timeout(stream_url,timeout):
+    """
+    Runs the JoqAmp/JOGL movie cube demo and returns how long it took to execute.
+
+    Execute terminated if run time exceeds timeout.
+    """
+
+    def f():
+        run_with_timeout(["java","-cp",
+            "jar/jogl-all.jar:jar/gluegen-rt.jar:/usr/share/java/junit4.jar:jar/jogl-test.jar",
+            "com/jogamp/opengl/test/junit/jogl/demos/es2/av/MovieCube",
+            "-time","400000","-width","1980","-height","1080","-url",stream_url],timeout,
+            cwd=JOQAMP_DIR)
+    return time_function(f)
--- research/performance-evaluation-tools/util_test.py
+++ research/performance-evaluation-tools/util_test.py
@@ -7,25 +7,24 @@ Unit tests for the util module.
 import unittest
 
 import util
-
-BBB_ORIGINAL = "http://www.youtube.com/watch?v=YE7VzlLtp-4" # Blender Foundation / Big Buck Bunny 
+import videos
 
 class Test(unittest.TestCase):
 
 	@unittest.skip('')
 	def test_get_available_formats(self):
-		formats = util.get_available_formats(BBB_ORIGINAL)
+		formats = util.get_available_formats(videos.BBB_ORIGINAL)
 		print formats
 
 	@unittest.skip('')
 	def test_get_streaming_url(self):
-		formats = util.get_available_formats(BBB_ORIGINAL)
-		streaming_url = util.get_streaming_url(BBB_ORIGINAL,formats[0])
+		formats = util.get_available_formats(videos.BBB_ORIGINAL)
+		streaming_url = util.get_streaming_url(videos.BBB_ORIGINAL,formats[0])
 		print streaming_url
 	
 	def test_run_movie_cube(self):
-		formats = util.get_available_formats(BBB_ORIGINAL)
-		streaming_url = util.get_streaming_url(BBB_ORIGINAL,formats[0])
+		formats = util.get_available_formats(videos.BBB_ORIGINAL)
+        streaming_url = util.get_streaming_url(videos.BBB_ORIGINAL,formats[0])
 		util.run_movie_cube(streaming_url)
 
 if __name__ == "__main__":
