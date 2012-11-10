eb2f3d4620cdf455754f6b3bbfb86060d12d388f
CalumJEadie <calum@calumjeadie.com>
2012-11-10 13:36:25 +0000

    Added dict of video urls

---
+++ research/performance-evaluation-tools/videos.py
@@ -0,0 +1,2 @@
+BBB_ORIGINAL = "http://www.youtube.com/watch?v=YE7VzlLtp-4" # Blender Foundation / Big Buck Bunny 
+LG_CIRCLE = "http://www.youtube.com/watch?v=YECCWXbl31E" # CalumJEadie / 30s, most formats
