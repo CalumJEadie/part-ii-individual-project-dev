eddeb3aae53b378ec1e5a77a2dbb7e821bee4b19
Calum J. Eadie <calum@calumjeadie.com>
2012-12-29 21:22:11 +0000

    Added more example from zetcode tut

---
+++ research/pyside/zetcode-tutorial/menu-toolbar/all.py
@@ -0,0 +1,43 @@
+import sys
+from PySide import QtGui
+
+class Example(QtGui.QMainWindow):
+    
+    def __init__(self):
+        super(Example, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):               
+        
+        textEdit = QtGui.QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        exitAction = QtGui.QAction('Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAction)
+        
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Main window')    
+        self.show()
+        
+        
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
---
+++ research/pyside/zetcode-tutorial/menu-toolbar/menu.py
@@ -0,0 +1,37 @@
+import sys
+from PySide import QtGui
+
+class Example(QtGui.QMainWindow):
+    
+    def __init__(self):
+        super(Example, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):               
+        
+        exitAction = QtGui.QAction(QtGui.QIcon('exit.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+        
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Menubar')    
+        self.show()
+        
+        
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
