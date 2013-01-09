b4801c5fb1820f8f8628b131e510005be355351b
Calum J. Eadie <calum@calumjeadie.com>
2013-01-09 22:05:35 +0000

    Implemented translation using normal form for most small language components

--- app/models/language.py
+++ app/models/language.py
@@ -13,11 +13,147 @@ of an education programming language.
 
 Classes representing method invocations are named using module, class and
 method.
+
+To minimise implementation and make output close to normal Python for education
+purposes Python's variable system is used directly.
+
+Hence the language is dynamically typed.
+
+Function class based on Scala's anonymous functions.
 """
 
 import app.api.youtube
 
-class Act():
+def partition_on_last_newline(text):
+    """
+    Splits text into lines up to last line and the last line.
+
+    >>> print partition_on_last_newline("a")
+    ('', 'a')
+    >>> print partition_on_last_newline("a\\nb")
+    ('a', 'b')
+    >>> print partition_on_last_newline("a\\nb\\nc")
+    ('a\\nb', 'c')
+    """
+    before,sep,after = text.rpartition("\n")
+    return before,after
+
+def get_fresh_variable_name():
+    get_fresh_variable_name.count += 1
+    return "tmp_%s" % get_fresh_variable_name.count
+get_fresh_variable_name.count = 0
+
+class LanguageComponent(object):
+    """Base class for all components of the language."""
+    
+    def translate(self):
+        """
+        Returns a translation of language component in linear Python code.
+
+        Indentation, blankspace and other details depend on the particular type
+        of language component.
+
+        :return: Linear Python code.
+        :rtype: string
+        """
+        raise NotImplementedError
+
+
+
+class Expression(LanguageComponent):
+    """Base class for all expressions in the language."""
+    pass
+
+class Operator2(Expression):
+    """Asbtract class for a 2-ary infix operator application"""
+
+    def __init__(self, operator_name, operand1, operand2):
+        self._operator_name = operator_name
+        self._operand1 = operand1
+        self._operand2 = operand2
+
+    def translate(self):
+        code = ""
+        operand1_var_name = get_fresh_variable_name()
+        code += SetVariableStatement(operand1_var_name,self._operand1).translate()
+        operand2_var_name = get_fresh_variable_name()
+        code += SetVariableStatement(operand2_var_name,self._operand2).translate()
+        code += "%s %s %s" % (operand1_var_name, self._operator_name,
+            operand2_var_name)
+        return code
+
+class Function1(Expression):
+    """Abstract class for a 1-ary function application."""
+
+    def __init__(self, function_name, operand1):
+        self._function_name = function_name
+        self._operand1 = operand1
+
+    def translate(self):
+        code = ""
+        operand1_var_name = get_fresh_variable_name()
+        code += SetVariableStatement(operand1_var_name,self._operand1).translate()
+        code += "%s(%s)" % (self._function_name, operand1_var_name)
+        return code
+
+class Function2(Expression):
+    """Abstract class for a 2-ary function application."""
+
+    def __init__(self, function_name, operand1, operand2):
+        self._function_name = function_name
+        self._operand1 = operand1
+        self._operand2 = operand2
+
+    def translate(self):
+        code = ""
+        operand1_var_name = get_fresh_variable_name()
+        code += SetVariableStatement(operand1_var_name,self._operand1).translate()
+        operand2_var_name = get_fresh_variable_name()
+        code += SetVariableStatement(operand2_var_name,self._operand2).translate()
+        code += "%s(%s, %s)" % (self._function_name, operand1_var_name, 
+            operand2_var_name)
+        return code
+
+class Function3(Expression):
+    """Abstract class for a 3-ary function application."""
+
+    def __init__(self, function_name, operand1, operand2, operand3):
+        self._function_name = function_name
+        self._operand1 = operand1
+        self._operand2 = operand2
+        self._operand3 = operand3
+
+    def translate(self):
+        code = ""
+        operand1_var_name = get_fresh_variable_name()
+        code += SetVariableStatement(operand1_var_name,self._operand1).translate()
+        operand2_var_name = get_fresh_variable_name()
+        code += SetVariableStatement(operand2_var_name,self._operand2).translate()
+        operand3_var_name = get_fresh_variable_name()
+        code += SetVariableStatement(operand3_var_name,self._operand3).translate()
+        code += "%s(%s, %s, %s)" % (self._function_name, operand1_var_name, 
+            operand2_var_name, operand3_var_name)
+        return code
+
+class InstanceMethod0(Expression):
+    """Abstrat class for a 0-ary instance method application."""
+
+    def __init__(self, instance_expr, method_name):
+        """
+        :type instance_expr: <:Expression
+        :type method_name: string
+        """
+        self._instance_expr = instance_expr
+        self._method_name = method_name
+
+    def translate(self):
+        code = ""
+        instance_var_name = get_fresh_variable_name()
+        code += SetVariableStatement(instance_var_name,self._instance_expr).translate()
+        code += "%s.%s()" % (instance_var_name, self._method_name)
+        return code
+
+class Act(LanguageComponent):
     """
     Highest level container. Analagous to a program.
     """
@@ -25,41 +161,64 @@ class Act():
     def __init__(self, scenes):
         self._scenes = scenes
 
-class Scene(object):
+    def translate(self):
+        code = ""
+        for scene in self._scenes:
+            code += scene.translate()
+
+class Scene(LanguageComponent):
     """
     Analagous to a basic block.
     """
     
     def __init__(self, title, comment, duration):
         """
-        :type title: <:TextExpression
-        :type comment: <:TextExpression
+        :type title: string
+        :type comment: string
         :type duration: <:NumberExpression
         """
         self._title = title
         self._comment = comment
         self._duration = duration
 
-class VideoScene(Scene):
+    def translate(self):
+        """
+        Preconditions:
+        - Will follow a new line.
+        Postconditions:
+        - Will end in a new line.
+        """
+        code = ""
+        code += CommentStatement(self._title).translate()
+        if self._comment != "":
+            code += CommentStatement("").translate()
+            code += CommentStatement(self._comment).translate()
+        return code
+
+class VideoScene(Scene,Function3):
 
     def __init__(self, title, comment, duration, offset, source):
         """
-        :type title: <:TextExpression
-        :type comment: <:TextExpression
+        :type title: string
+        :type comment: string
         :type duration: <:NumberExpression
         :type offset: <:NumberExpression
         :type source: <:VideoExpression
         """
         super(VideoScene, self).__init__(title, comment, duration)
-        self._offset = offset
-        self._source = source
+        Function3.__init__(self, "videoplayer", source, offset, duration)
+
+    def translate(self):
+        code = super(VideoScene, self).translate()
+        code += Function3.translate(self)
+        return code
 
 class ImageScene(Scene):
 
     def __init__(self, title, comment, duration, offset, source):
         """
-        :type title: <:TextExpression
-        :type comment: <:TextExpression
+        :type title: string
+        :type comment: string
         :type duration: <:NumberExpression
         :type offset: <:NumberExpression
         :type source: <:VideoExpression
@@ -72,17 +231,66 @@ class TextScene(Scene):
 
     def __init__(self, title, comment, duration, text):
         """
-        :type title: <:TextExpression
-        :type comment: <:TextExpression
+        :type title: string
+        :type comment: string
         :type duration: <:NumberExpression
         :type text: <:TextExpression
         """
         super(TextScene, self).__init__(title, comment, duration)
         self._text = text
 
-class Expression(object):
-    """Base class for expressions."""
-    pass
+    def translate(self):
+        code = super(TextScene, self).translate()
+        text_var_name = get_fresh_variable_name()
+        code += SetVariableStatement(text_var_name,self._text).translate()
+        code += "display(%s,duration)" % text_var_name
+        return code
+
+class Statement(LanguageComponent):
+
+    def translate(self):
+        """
+        Preconditions:
+        - Will follow a new line.
+        Postconditions:
+        - Will end in a new line.
+        """
+        raise NotImplementedError
+
+class CommentStatement(Statement):
+
+    def __init__(self, text):
+        self._text = text
+
+    def translate(self):
+        return "# %s\n" % self._text
+
+class SetVariableStatement(Statement):
+
+    def __init__(self, name, value):
+        """
+        :type name: string
+        :type value: <: TextExpression | NumberExpression | VideoExpression | VideoCollectionExpression
+        """
+        self._name = name
+        self._value = value
+
+    def translate(self):
+        value_code = self._value.translate()
+        up_to, last = partition_on_last_newline(value_code)
+        code = up_to
+        code += "%s = %s\n" % (self._name,last)
+        return code
+
+class GetVariableExpression(LanguageComponent):
+
+    def __init__(self, name):
+        """
+        :type name: string
+        """
+
+    def translate(self):
+        return self._name
 
 class TextExpression(Expression):
     """Base class for expressions that evaluate to type Text."""
@@ -96,33 +304,43 @@ class TextValue(TextExpression):
         """
         self._text = text
 
-class YoutubeVideoGetTitleOperation(TextExpression):
+    def translate(self):
+        return "'%s'" % self._text
+
+class YoutubeVideoGetTitle(TextExpression):
+
+    def __init__(self,video):
+        """
+        :type video: <:VideoExpression
+        """
+        self._video = video
+
+    def translate(self):
+        code = ""
+        video_var_name = get_fresh_variable_name()
+        code += SetVariableStatement(video_var_name,self._video).translate()
+        code += "\n%s.title()" % video_var_name
+        return code
+
+class YoutubeVideoGetDescription(InstanceMethod0, TextExpression):
 
     def __init__(self,video_expr):
         """
         :type video_expr: <:VideoExpression
         """
-        self._video_expr = video_expr
-
-class YoutubeVideoGetDescriptionOperation(TextExpression):
-
-    def __init__(self,video_expr):
-        """
-        :type video_expr: <:VideoExpression
-        """
-        self._video_expr = video_expr
+        super(YoutubeVideoGetDescription, self).__init__(video_expr, "description")
 
 class NumberExpression(Expression):
     """Base class for expressions that evaluate to type Number."""
     pass
 
-class YoutubeVideoGetDurationOperation(NumberExpression):
+class YoutubeVideoGetDuration(InstanceMethod0, NumberExpression):
 
     def __init__(self,video_expr):
         """
         :type video_expr: <:VideoExpression
         """
-        self._video_expr = video_expr
+        super(YoutubeVideoGetDuration, self).__init__(video_expr, "duration")
 
 class NumberValue(NumberExpression):
 
@@ -132,25 +350,36 @@ class NumberValue(NumberExpression):
         """
         self._number = number
 
-class GetRandomNumberBetweenIntervalOperation(NumberExpression):
+    def translate(self):
+        return str(self._number)
+
+class GetRandomNumberBetweenInterval(Function2, NumberExpression):
 
     def __init__(self,lower_number_expr,higher_number_expr):
         """
         :type lower_number_expr: <:NumberExpression
         :type higher_number_expr: <:NumberExpression
         """
-        self._lower_number_expr = lower_number_expr
-        self._higher_number_expr = higher_number_expr
+        super(GetRandomNumberBetweenInterval, self).__init__(
+            "random.uniform",
+            lower_number_expr,
+            higher_number_expr
+        )
 
-class SubtractOperation(NumberExpression):
+class Add(Operator2):
 
-    def __init__(self,number_expr_1,number_expr_2):
-        """
-        :type number_expr_1: <:NumberExpression
-        :type number_expr_2: <:NumberExpression
-        """
-        self._number_expr_1 = number_expr_1
-        self._number_expr_2 = number_expr_2
+    def __init__(self,op1,op2):
+        super(Add,self).__init__("+", op1, op2)
+
+class Subtract(Operator2):
+
+    def __init__(self,op1,op2):
+        super(Subtract,self).__init__("-", op1, op2)
+
+class Multiply(Operator2):
+
+    def __init__(self,op1,op2):
+        super(Multiply,self).__init__("*", op1, op2)
 
 class VideoExpression():
     """Base class for expressions that evaluate to type Video."""
@@ -164,30 +393,42 @@ class VideoValue(VideoExpression):
         """
         self._video = video
 
+    def translate(self):
+        return "Video('%s')" % self._video
+
 class VideoCollectionExpression():
     """Base class for expressions that evaluate to type VideoCollection."""
     pass
 
-class YoutubeVideoGetRelatedOperation(VideoCollectionExpression):
+    def __init__(self,video_collection):
+        """
+        :type video_collection: VideoCollection
+        """
+        self._video_collection = video_collection
+
+    def translate(self):
+        return "VideoCollection('%s')" % self._video_collection
+
+class YoutubeVideoGetRelated(InstanceMethod0, VideoCollectionExpression):
 
     def __init__(self,video_expr):
         """
         :type video_expr: <:VideoExpression
         """
-        self._video_expr = video_expr
+        super(YoutubeVideoGetRelated, self).__init__(video_expr, "related")
 
-class YoutubeSearchOperation(VideoCollectionExpression):
+class YoutubeSearch(Function1, VideoCollectionExpression):
 
     def __init__(self,text_expr):
         """
         :type text_expr: <:TextExpression
         """
-        self._text_expr = text_expr
+        super(YoutubeSearch, self).__init__(text_expr, "youtube.search")
 
-class YoutubeVideoCollectionRandomOperation(VideoCollectionExpression):
+class YoutubeVideoCollectionRandom(InstanceMethod0, VideoCollectionExpression):
 
     def __init__(self,video_collection_expr):
         """
         :type video_collection_expr: <:VideoCollectionExpression
         """
-        self._video_collection_expr = video_collection_expr
+        super(YoutubeVideoCollectionRandom, self).__init__(video_collection_expr, "random")
--- app/test/models/test_language.py
+++ app/test/models/test_language.py
@@ -28,32 +28,100 @@ class SmartMusicPlayerTest(unittest.TestCase):
         curr_video = VideoExpression()
 
         scene1 = TextScene(
-            title = TextValue("Show video title"),
-            comment = TextValue(""),
+            title = "Show video title",
+            comment = "",
             duration = NumberValue(2),
-            text = YoutubeVideoGetTitleOperation(curr_video)
+            text = YoutubeVideoGetTitle(curr_video)
         )
 
         scene2 = TextScene(
-            title = TextValue("Show video description"),
-            comment = TextValue(""),
+            title = "Show video description",
+            comment = "",
             duration = NumberValue(2),
-            text = YoutubeVideoGetDescriptionOperation(curr_video)
+            text = YoutubeVideoGetDescription(curr_video)
         )
 
         clip_duration = NumberValue(1)
-        video_duration = YoutubeVideoGetDurationOperation(curr_video)
-        clip_offset = GetRandomNumberBetweenIntervalOperation(
-            NumberValue(0), SubtractOperation(video_duration,clip_duration)
+        video_duration = YoutubeVideoGetDuration(curr_video)
+        clip_offset = GetRandomNumberBetweenInterval(
+            NumberValue(0), Subtract(video_duration,clip_duration)
         )
 
         scene3 = VideoScene(
-            title = TextValue("Play video"),
-            comment = TextValue(""),
+            title = "Play video",
+            comment = "",
             duration = clip_duration,
             offset = clip_offset,
             source = curr_video
         )
+
+    def test_translate_inner_loop_scenes(self):
+        """
+        Tests translating scenes describing inner loop from the example.
+        """
+
+        curr_video = VideoValue("http://www.youtube.com/watch?v=9bZkp7q19f0") # PSY - GANGNAM STYLE
+
+        scene1 = TextScene(
+            title = "Show video title",
+            comment = "",
+            duration = NumberValue(2),
+            text = YoutubeVideoGetTitle(curr_video)
+        )
+        print scene1.translate()
+
+        scene2 = TextScene(
+            title = "Show video description",
+            comment = "",
+            duration = NumberValue(2),
+            text = YoutubeVideoGetDescription(curr_video)
+        )
+        print scene2.translate()
+
+        clip_duration = NumberValue(1)
+        video_duration = YoutubeVideoGetDuration(curr_video)
+        clip_offset = GetRandomNumberBetweenInterval(
+            NumberValue(0), Subtract(video_duration,clip_duration)
+        )
+
+        scene3 = VideoScene(
+            title = "Play video",
+            comment = "",
+            duration = clip_duration,
+            offset = clip_offset,
+            source = curr_video
+        )
+        print scene3.translate()
+
+    def test_operators(self):
+        """
+        Tests of representation of standard Python operators.
+        """
+        exec Add(NumberValue(1),NumberValue(2)).translate()
+        exec Subtract(NumberValue(1),NumberValue(2)).translate()
+        exec Multiply(NumberValue(1),NumberValue(2)).translate()
+
+    def test_instance_methods(self):
+        """
+        Tests of representation of instance methods.
+        """
+        InstanceMethod0(
+            VideoValue("http://www.youtube.com/watch?v=9bZkp7q19f0"),
+            "title"
+        ).translate()
+
+        YoutubeVideoGetTitle(
+            VideoValue("http://www.youtube.com/watch?v=9bZkp7q19f0")
+        ).translate()
+
+        InstanceMethod0(
+            VideoValue("http://www.youtube.com/watch?v=9bZkp7q19f0"),
+            "description"
+        ).translate()
+
+        YoutubeVideoGetDescription(
+            VideoValue("http://www.youtube.com/watch?v=9bZkp7q19f0")
+        ).translate()
 
 if __name__ == "__main__":
     unittest.main()
