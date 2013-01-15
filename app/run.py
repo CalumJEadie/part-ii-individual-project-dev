2a6035b97632d893e2c8d079c0817ca269a78b1a
Calum J. Eadie <calum@calumjeadie.com>
2013-01-15 11:31:28 +0000

    Renamed run.py to avoid namespace clash

--- app/run.py
+++ app/run.py
@@ -6,26 +6,34 @@ import argparse
 
 import ui.text_editor
 import ui.graphical_editor
+import ui.basic_graphical_editor
+import ui.title_editor
 
 logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # Possible editors.
 TEXT_EDITOR = "text"
 GRAPHICAL_EDITOR = "graphical"
+BASIC_GRAPHICAL_EDITOR = "basic_graphical"
+TITLE_EDITOR = "title"
         
 def main():
 
     # Process command line.
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--editor",default=TEXT_EDITOR,choices=[TEXT_EDITOR,GRAPHICAL_EDITOR],help="specifies type of editor")
+    parser.add_argument("--editor",default=TEXT_EDITOR,choices=[TEXT_EDITOR, GRAPHICAL_EDITOR, BASIC_GRAPHICAL_EDITOR, TITLE_EDITOR],help="specifies type of editor")
     args = parser.parse_args(sys.argv[1:])
 
     # Start application
     app = QtGui.QApplication(sys.argv)
     if args.editor == TEXT_EDITOR:
         e = ui.text_editor.TextEditor()
-    else:
+    elif args.editor == GRAPHICAL_EDITOR:
         e = ui.graphical_editor.GraphicalEditor()
+    elif args.editor == BASIC_GRAPHICAL_EDITOR:
+        e = ui.basic_graphical_editor.BasicGraphicalEditor()
+    else:
+        e = ui.title_editor.TitleEditor()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
