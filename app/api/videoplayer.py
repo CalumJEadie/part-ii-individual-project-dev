07632a5094c1036244e2fe8da62202c43f5c2c60
Calum J. Eadie <calum@calumjeadie.com>
2013-01-15 11:30:51 +0000

    Added OMXPlayer detection to videoplayer API.

--- app/api/videoplayer.py
+++ app/api/videoplayer.py
@@ -4,10 +4,19 @@ Videoplayer API.
 
 import logging
 import time
+import distutils.spawn
+
+import pyomxplayer
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 def play(video,offset,duration):
     logger.info("play(video=%s,offset=%s,duration=%s)",video,offset,duration)
-    time.sleep(duration)
+    if pyomxplayer.is_omxplayer_available():
+        p = pyomxplayer.OMXPlayer(video)
+        time.sleep(duration)
+        p.stop()
+    else:
+        log.info("OMXPlayer not available, calling `time.sleep`.")
+        time.sleep(duration)
