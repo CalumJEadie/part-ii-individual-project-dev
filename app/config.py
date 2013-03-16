95aeda16a7319dd0269467ceb4457d843001e910
Calum J. Eadie <calum@calumjeadie.com>
2013-03-16 03:49:46 +0000

    Parametised config on environment

--- app/config.py
+++ app/config.py
@@ -5,20 +5,33 @@ Application configuration.
 import os.path
 from os.path import join
 
+WINTERFELL,ED,AL = range(0,3)
+env = WINTERFELL
+
 # General
 
 APP_NAME = "Evelyn"
 SCRIPT_EXTENSION = "ev"
 #APP_DIR = os.path.expanduser("~/evelyn")
-APP_DIR = "/media/NARSIL/evelyn"
+#APP_DIR = "/media/NARSIL/evelyn"
+APP_DIR = {
+    WINTERFELL: os.path.expanduser("~/evelyn"),
+    ED: "/media/NARSIL/evelyn",
+    AL: "/media/NARSIL/evelyn",
+}[env]
 SCREENSHOT_FORMAT = "png"
 
 # API - Video Cache
 
 #CACHE_DIR = "/tmp/diss/videocache"
 CACHE_DIR = join(APP_DIR, "videocache")
-# FORMAT = "worst"
-FORMAT = "best"
+#FORMAT = "worst"
+#FORMAT = "best"
+FORMAT = {
+    WINTERFELL: "worst",
+    ED: "worst",
+    AL: "worst",
+}[env]
 
 # UI - Graphical Editor
 
